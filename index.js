341fd5636073a4dddb1b0274bf7eedbc8e50bede
princexz <prince_solomon@yahoo.com>
2023-01-08 15:27:26 -0800

    javascript functions

---
+++ index.js
@@ -0,0 +1,13 @@
+function prince(name, height, country) {
+  this.name = name;
+  this.height = height;
+  this.country = country;
+}
+
+let prince1 = new prince("solomon", "7-8", "Lagos Nigeria");
+console.log(prince1);
+
+function alert() {
+  alert("How are you doing?");
+}
+console.log("Hello how are you Prince?");
